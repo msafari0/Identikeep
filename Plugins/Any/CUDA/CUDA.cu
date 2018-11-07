aa5218fa0b78cf9b8073cab0cd5ac6884674883b
acolomb3 <a.colombo@cineca.it>
2018-11-07 10:45:36 +0100

    CUDA plugin updated (CUDA version info added)

--- Plugins/Any/CUDA/CUDA.cu
+++ Plugins/Any/CUDA/CUDA.cu
@@ -17,11 +17,16 @@ bool CUDA::Collect(int argc, char *argv[])
   if(nDevices>0){
 
     cudaDeviceProp prop[nDevices];
-
+    int runtimeVersion=0;
+    int driverVersion=0;
+    
+    cudaDriverGetVersion(&driverVersion);
+    cudaRuntimeGetVersion(&runtimeVersion);
+    
     std::vector<std::string> 	dev_list;
     std::vector<float> 	   	totalGlobalMem_list;
     std::vector<int> 		clockRate_list;
-    std::vector<std::string> 	version_list;
+    std::vector<std::string> 	compcap_list;
 
     for (int i = 0; i < nDevices; i++){
         cudaGetDeviceProperties(&prop[i],i);
@@ -31,7 +36,7 @@ bool CUDA::Collect(int argc, char *argv[])
         clockRate_list.push_back(prop[i].clockRate);
         
         std::string vv=std::to_string(prop[i].major)+"."+std::to_string(prop[i].minor);
-        version_list.push_back(vv);
+        compcap_list.push_back(vv);
     }
 
 
@@ -39,14 +44,21 @@ bool CUDA::Collect(int argc, char *argv[])
         Item<std::string> idev_list = Item<std::string>("cudaDevice", match::list | match::node | match::world, dev_list);
         m_items.strings.push_back(idev_list);
 
-        Item<std::string> iversion = Item<std::string>("cudaVersion", match::list | match::node | match::world, version_list);
-        m_items.strings.push_back(iversion);
+        Item<std::string> icompcap = Item<std::string>("cudaComputeCapability", match::list | match::node | match::world, compcap_list);
+        m_items.strings.push_back(icompcap);
 
         Item<int> iclock = Item<int>("cudaClock", "KHz", match::list | match::node | match::world, clockRate_list);
         m_items.integers.push_back(iclock);
 
         Item<float> imem = Item<float>("cudaTotMem", "MB", match::list| match::node | match::world, totalGlobalMem_list);
         m_items.floats.push_back(imem);
+        
+        Item<int> iDriverVersion = Item<int>("cudaDriverVersion",  match::list | match::node | match::world, driverVersion);
+        m_items.integers.push_back(iDriverVersion);
+        
+        Item<int> iRuntimeVersion = Item<int>("cudaRuntimeVersion",  match::list | match::node | match::world, runtimeVersion);
+        m_items.integers.push_back(iRuntimeVersion);
+        
   }
   else{
         Item<std::string> idev_list = Item<std::string>("cudaDevice", match::list | match::node | match::world, "No NVIDIA GPU detected");
